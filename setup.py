1c30884ca2b98c2b01385593b32fa66ffc3d2b4c
unknown <jayshankar15/mlproject>
2023-12-12 22:46:04 +0530

    setup file added

---
+++ setup.py
@@ -0,0 +1,31 @@
+
+
+from setuptools import find_packages,setup
+
+
+Hyphen_dot = '-e .'
+
+def get_requirements(file_path:str):
+    '''
+    This function will returns the requirements
+    '''
+
+    requirements= []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n","") for req in requirements]
+
+        if Hyphen_dot in requirements:
+            requirements.remove(Hyphen_dot)
+    return requirements
+
+
+setup(
+name ='mlproject',
+author= 'Jay Shankar',
+version='0.0.1',
+author_email = 'jassshankar79@gmail.com',
+packages = 'find_packages()',
+install_requires = get_requirements('requirements.txt')
+
+) 
