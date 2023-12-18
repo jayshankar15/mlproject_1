120fe5fc36c084746cf23b59028d0f244d436b3c
unknown <jayshankar15/mlproject>
2023-12-18 20:54:44 +0530

    The second commit

--- setup.py
+++ setup.py
@@ -1,15 +1,11 @@
-
-
 from setuptools import find_packages,setup
-
-
+from typing import List
+packages=find_packages()
 Hyphen_dot = '-e .'
-
-def get_requirements(file_path:str):
+def get_requirements(file_path:str)->List[str]:
     '''
     This function will returns the requirements
     '''
-
     requirements= []
     with open(file_path) as file_obj:
         requirements = file_obj.readlines()
@@ -25,7 +21,6 @@ name ='mlproject',
 author= 'Jay Shankar',
 version='0.0.1',
 author_email = 'jassshankar79@gmail.com',
-packages = 'find_packages()',
+packages = find_packages(),
 install_requires = get_requirements('requirements.txt')
-
 ) 
